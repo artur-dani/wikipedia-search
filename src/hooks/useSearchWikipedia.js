1c003139cd7f03fd7a000db5b94f51cecd9cbfde
Artur Dani <darthur83@hotmail.com>
2022-06-07 02:33:24 +0200

    remove useFetch as extra layer, use useSerachWikipedia instead

--- src/hooks/useSearchWikipedia.js
+++ src/hooks/useSearchWikipedia.js
@@ -1,27 +1,40 @@
 import { useState, useEffect } from "react";
 import { API_URL } from "../constants";
 import qs from "qs";
-import useFetch from "./useFetch";
+import axios from "axios";
 import { stripHtml } from "../utils";
 
-const useSearchWikipedia = (searchPhrase = "test") => {
+const useSearchWikipedia = (searchPhrase) => {
   const [articles, setArticles] = useState([]);
-  const { data, loading, error } = useFetch(
-    `${API_URL}search/page?${qs.stringify({ q: searchPhrase, limit: 5 })}`
-  );
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (data) {
-      const articles = data.pages.map((page) => ({
-        id: page.id,
-        title: page.title,
-        description: stripHtml(page.excerpt),
-        url: `https://en.wikipedia.org/wiki/${page.key}`,
-      }));
-
-      setArticles(articles);
+    if (!searchPhrase) {
+      return;
     }
-  }, [data]);
+    axios
+      .get(
+        `${API_URL}search/page?${qs.stringify({ q: searchPhrase, limit: 5 })}`
+      )
+      .then((res) => {
+        if (res.data) {
+          const articles = res.data.pages.map((page) => ({
+            id: page.id,
+            title: page.title,
+            description: stripHtml(page.excerpt),
+            url: `https://en.wikipedia.org/wiki/${page.key}`,
+          }));
+
+          setArticles(articles);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [searchPhrase]);
 
   return { articles, loading, error };
 };
